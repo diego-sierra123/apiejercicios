7307fc792ecf1de9c068a395bf0d6b25f392d316
Diego Alejandro Sierra Hernández <diego.sierra@sistemasgyg.com.co>
2024-02-15 15:20:22 -0500

    first commit

---
+++ ejerciciosapi/ejerciciosapi/Controllers/Ejercicioone.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ejerciciosapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ejercicioone : ControllerBase
+    {
+        private bool EsNumeroPar(long respuesta)
+        {
+            return respuesta % 2 == 0;
+        }
+
+        [HttpGet("{numero}")]
+        public ActionResult<string> Get(string numero)
+        {
+            if (long.TryParse(numero, out long respuesta))
+            {
+                if (EsNumeroPar(respuesta))
+                {
+                    return $"{respuesta} es un número par.";
+                }
+                else
+                {
+                    return $"{respuesta} es un número impar.";
+                }
+            }
+            else
+            {
+                return $"{numero} no es un número válido (Recuerda no ingresar signos, letras, simbolos, iconos, números decimales).";
+            }
+        }
+    }
+}
---
+++ ejerciciosapi/ejerciciosapi/Controllers/Ejerciciotwo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace ejerciciosapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ejerciciotwo : ControllerBase
+    {
+        private double promedio;
+        private double CalcularPromedio(long num1, long num2, long num3)
+        {
+            return (num1 + num2 + num3) / 3.0;
+        }
+
+        [HttpPost("datos")]
+        public IActionResult Post([Required] string num1, [Required] string num2, [Required] string num3)
+        {
+            if (long.TryParse(num1, out long numero1) && long.TryParse(num2, out long numero2) && long.TryParse(num3, out long numero3))
+            {
+                if (numero1 > 0 && numero2 > 0 && numero3 > 0)
+                {
+                    promedio = CalcularPromedio(numero1, numero2, numero3);
+                    var promedioRound = Math.Round(promedio, 2);
+                    if (promedioRound == promedio)
+                    {
+                        var resultado = new
+                        {
+                            Promedio = promedioRound,
+                            Mensaje = "El promedio es un número entero."
+                        };
+                        return Ok(resultado);
+                    }
+                    else
+                    {
+                        var resultado = new
+                        {
+                            Promedio = promedioRound,
+                            Mensaje = "El promedio es un número decimal."
+                        };
+                        return Ok(resultado);
+                    }
+                }
+                else
+                {
+                    var resultado = new
+                    {
+                        Promedio = "Error",
+                        Mensaje = "Por favor, ingrese solo números enteros positivos y no números negativos o 0."
+                    };
+                    return Ok(resultado);
+                }
+            }
+            else
+            {
+                var resultado = new
+                {
+                    Promedio = "Error",
+                    Mensaje = "Por favor, ingrese solo números enteros positivos y no símbolos, iconos, letras, números decimales, etc."
+                };
+                return Ok(resultado);
+            }
+        }
+    }
+}
